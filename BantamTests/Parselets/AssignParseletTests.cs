7f60e4ea1751dce86b2eef037942b132458ae209
Daniel <danieliozzi@gmail.com>
2014-12-15 19:49:19 +0200

    issing Files

---
+++ BantamTests/Parselets/AssignParseletTests.cs
@@ -0,0 +1,29 @@
+﻿using Bantam.Expressions;
+using Bantam.Paselets;
+using BantamTests.Support;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleParser;
+using Token = SimpleParser.Token<SimpleParser.TokenType>;
+
+namespace BantamTests.Parselets
+{
+    [TestClass]
+    public class AssignParseletTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var parselet = new AssignParselet();
+            var token = Token.New(TokenType.ASSIGN, "=");
+            var parser = new FakeParser(new NameExpression("a"),null);
+            var left = new NameExpression("A");
+            var p = parselet.Parse(parser, left, token);
+            var builder = new Builder();
+            p.Print(builder);
+            var x = builder.Build();
+            Assert.AreEqual("(A = a)", x);
+        }
+
+       
+    }
+}
---
+++ BantamTests/Support/FakesTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prefix = System.Tuple<SimpleParser.TokenType, SimpleParser.IPrefixParselet<SimpleParser.TokenType>>;
+using Infix = System.Tuple<SimpleParser.TokenType, SimpleParser.InfixParselet<SimpleParser.TokenType>>;
+
+namespace BantamTests.Support
+{
+    [TestClass]
+    public class FakesTests
+    {
+        [TestMethod]
+        public void FakeLexerTest()
+        {
+            var lexer = new FakeLexer("a");
+            var t = lexer.Next();
+            Assert.AreEqual("a", t.GetText());
+        }
+
+        [TestMethod]
+        public void FakeLexerTest2()
+        {
+            var lexer = new FakeLexer("ab");
+            Assert.AreEqual("a", lexer.Next().GetText());
+            Assert.AreEqual("b", lexer.Next().GetText());
+        }
+
+        [TestMethod]
+        public void FakeLexerTest3()
+        {
+            var lexer = new FakeLexer("");
+            //WARNING ! , null not ok 
+            Assert.AreEqual(null, lexer.Next().GetText());          
+        }
+
+        [TestMethod]
+        public void FakeBuilderTest()
+        {
+            var fake = new FakeExpression("x");
+            var builder = new FakeBuilder();
+            fake.Print(builder);
+            Assert.AreEqual("x", builder.Build());
+        }
+        
+    }
+
+    
+}
