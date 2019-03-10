eb6c795c75a6c7b6bb52f601cb231f0bf1f044a9
Daniel <danieliozzi@gmail.com>
2019-03-10 08:25:01 +0200

    TestLogger

---
+++ BantamTests/TestLogger.cs
@@ -0,0 +1,14 @@
+namespace BantamTests
+{
+    ///<summary>
+    ///  Does it work on Windows?
+    ///</summary>
+    public static class TestLogger
+    {
+        public static void Log(string message)
+        {
+            using (var writer = new System.IO.StreamWriter(System.Console.OpenStandardOutput()))
+                writer.WriteLine(message);
+        }
+    }
+}
