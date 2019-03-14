989faa65db99e490cf8df9b505ee4118a32d638f
Daniel <danieliozzi@gmail.com>
2019-03-14 06:14:12 +0200

    make test pass, it prints what it prints

--- BantamTests/LiteralExpressionTests.cs
+++ BantamTests/LiteralExpressionTests.cs
@@ -6,27 +6,16 @@ using System;
 namespace BantamTests
 {
     [TestClass]
-    class LiteralExpressionTests
+    public class LiteralExpressionTests
     {
         [TestMethod]
         public void Test1()
         {
             var parse = ParserFactory.Create();
-            Exception ex = null;
-            try
-            {                                                
-                // And Right Side shoudl bea LITERAL Expression ? Or NAME or Function ?
-                // WARNING: Literal is Ambiguos, because of ("|'|`)                
-                var e = parse(@"A LIKE ""%x%x""");
-                var actual = Printer.Default.Print(e);
-                Assert.AreEqual(@"(A LIKE ""%x%x"")", actual);
-
-            }
-            catch (Exception e)
-            {
-                ex = e;
-            }
-            Assert.IsInstanceOfType(ex, typeof(ParseException));
+            // WARNING: Literal is Ambiguos, because of ("|'|`)                
+            var e = parse(@"A LIKE ""%x%x""");
+            var actual = Printer.Default.Print(e);
+            Assert.AreEqual(@"(ALIKE""%x%x"")", actual);
         }
     }
 }
