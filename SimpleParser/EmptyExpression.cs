2ad60d21ac6390c1f610c796f5a1e13a046d7ff9
Daniel <danieliozzi@gmail.com>
2019-03-17 08:43:36 +0200

    remove unused code

--- SimpleParser/EmptyExpression.cs
+++ SimpleParser/EmptyExpression.cs
@@ -3,11 +3,5 @@
     public class EmptyExpression<T> : ISimpleExpression<T>
     {
         public IToken<T> Token { get; } = SimpleParser.Token.Empty<T>();
-
-
-        public void Visit(IExpressionVisitor<T> visitor)
-        {
-            visitor.Visit(this);
-        }
     }
 }
